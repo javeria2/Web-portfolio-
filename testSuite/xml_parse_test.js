37fb0a42e6b1c957d76d0e68064ba403a9f35464
Sanchay Javeria <javeria2@illinois.edu>
2016-10-12 04:01:27 -0500

    tests now fully working

--- testSuite/xml_parse_test.js
+++ testSuite/xml_parse_test.js
@@ -9,34 +9,72 @@ var logJSON = [], listJSON = [];
 //this data-structure are for holding the relevant bits
 var infoList = [{}];
 
+//test parsing of file
 describe('parse xml files', function(){
-	this.timeout(150000);
-	it('should parse correctly', function(){
-		this.timeout(150000);
+	this.timeout(140000);
+	it('should parse correctly', function(done){
+		this.timeout(140000);
+		setTimeout(function(){
+			done();
+		}, 140000);
 		xmlParser.parse(__dirname + "/../xml/svn_log_test.xml", function(err,  log) {
 			if(err) {
-				assert.equal(true, false);
+				return done(err);
 			}
       		this.logJSON = log["log"]["logentry"];
       		xmlParser.parse(__dirname + "/../xml/svn_list_test.xml", function(err, list) {
       			if(err) {
-      				assert.equal(true, false);
+      				return done(err);
       			}
         		this.listJSON = list["lists"]["list"][0]["entry"];
 
         		//now extract relevant bits
         		extractInformation.extractInfo(this.listJSON, this.logJSON, function(err, information){
         			if(err) {
-        				assert.equal(true, false);
+        				return done(err);
         			}
           			this.infoList = information;
-          			console.log(this.logJSON);
-    				console.log(this.listJSON);
-    				console.log(this.infoList);
 
     				assert.equal(this.logJSON[0]['author'][0], 'javeria2');
-    				assert.equal(this.listJSON[1]['name'][0], 'Assignment0/.DS_Store');
-    				assert.equal(this.infoList[0]['version'], '2592');
+    				assert.equal(this.listJSON[1]['name'][0], 'Assignment2.0/CSAir');
+    				assert.equal(this.infoList[0]['version'], '6194');
+    				done();
+        		});
+      		});
+    	});
+	});
+});
+
+// test multiple commits of same file and multiple commits of different files
+describe('parse xml files', function(){
+	this.timeout(140000);
+	it('should parse correctly', function(done){
+		this.timeout(140000);
+		setTimeout(function(){
+			done();
+		}, 140000);
+		xmlParser.parse(__dirname + "/../xml/svn_log_test.xml", function(err,  log) {
+			if(err) {
+				return done(err);
+			}
+      		this.logJSON = log["log"]["logentry"];
+      		xmlParser.parse(__dirname + "/../xml/svn_list_test.xml", function(err, list) {
+      			if(err) {
+      				return done(err);
+      			}
+        		this.listJSON = list["lists"]["list"][0]["entry"];
+
+        		//now extract relevant bits
+        		extractInformation.extractInfo(this.listJSON, this.logJSON, function(err, information){
+        			if(err) {
+        				return done(err);
+        			}
+          			this.infoList = information;
+          			console.log(this.infoList[0]['files']['versions']);
+    				assert.equal(this.logJSON[0]['author'][0], 'javeria2');
+    				assert.equal(this.listJSON[1]['name'][0], 'Assignment2.0/CSAir');
+    				assert.equal(this.infoList[0]['version'], '6194');
+    				done();
         		});
       		});
     	});
